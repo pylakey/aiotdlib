78d4b820dffa732163d9edd3ab2effdb769680d8
pylakey <pylakey@protonmail.com>
2024-01-01 23:57:09 +0300

    Remove Pydantic v2 check and refactor imports
    The Pydantic v2 compatibility check has been removed from the aiotdlib initialization file. Also various module imports were refactored for readability without a change in functionality.

--- aiotdlib/__init__.py
+++ aiotdlib/__init__.py
@@ -1,56 +1,43 @@
 __version__ = "0.22.0"
 
-import pydantic
-
-if pydantic.VERSION.startswith("2"):
-    raise NotImplementedError("Aiotdlib currently does not support Pydantic V2")
-
-from .client import (
-    Client,
-    ClientOptions,
-    ClientParseMode,
-    ClientProxySettings,
-    ClientProxyType,
-)
-from .filters import (
-    AllFilter,
-    AnimationFilter,
-    AudioFilter,
-    BaseFilter,
-    BaseObjectFilter,
-    BotCommandFilter,
-    ContactFilter,
-    DiceFilter,
-    DocumentFilter,
-    FilterCallable,
-    Filters,
-    GameFilter,
-    InvertedFilter,
-    InvoiceFilter,
-    LocationFilter,
-    MergedFilter,
-    MessageFilter,
-    PhotoFilter,
-    PollFilter,
-    RegexFilter,
-    StickerFilter,
-    TextFilter,
-    UnsupportedFilter,
-    VenueFilter,
-    VideoFilter,
-    VideoNoteFilter,
-    VoiceNoteFilter,
-    XORFilter,
-)
-from .handlers import (
-    Handler,
-    HandlerCallable,
-)
+from .client import Client
+from .client import ClientOptions
+from .client import ClientParseMode
+from .client import ClientProxySettings
+from .client import ClientProxyType
+from .filters import AllFilter
+from .filters import AnimationFilter
+from .filters import AudioFilter
+from .filters import BaseFilter
+from .filters import BaseObjectFilter
+from .filters import BotCommandFilter
+from .filters import ContactFilter
+from .filters import DiceFilter
+from .filters import DocumentFilter
+from .filters import FilterCallable
+from .filters import Filters
+from .filters import GameFilter
+from .filters import InvertedFilter
+from .filters import InvoiceFilter
+from .filters import LocationFilter
+from .filters import MergedFilter
+from .filters import MessageFilter
+from .filters import PhotoFilter
+from .filters import PollFilter
+from .filters import RegexFilter
+from .filters import StickerFilter
+from .filters import TextFilter
+from .filters import UnsupportedFilter
+from .filters import VenueFilter
+from .filters import VideoFilter
+from .filters import VideoNoteFilter
+from .filters import VoiceNoteFilter
+from .filters import XORFilter
+from .handlers import Handler
+from .handlers import HandlerCallable
 from .middlewares import MiddlewareCallable
-from .tdjson import (
-    TDJson,
-    TDLibLogVerbosity,
-)
+from .tdjson import TDJson
+from .tdjson import TDLibLogVerbosity
 from .utils import PendingRequest
 
 __all__ = [
