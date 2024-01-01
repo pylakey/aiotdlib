0a72fbeb3caade90590fce473ce2f4ca8fd31ae7
pylakey <pylakey@protonmail.com>
2024-01-01 16:58:08 +0300

    Refactor aiotdlib/api/types/base.py for Pydantic upgrade
    Aligned aiotdlib/api/types/base.py with latest Pydantic standards by refactoring import statements and updating data types. Replaced confloat and conint from pydantic with Annotated from typing and changed BaseConfig with ConfigDict

--- aiotdlib/api/types/base.py
+++ aiotdlib/api/types/base.py
@@ -2,39 +2,32 @@ from __future__ import annotations
 
 import logging
 import typing
-from typing import (
-    Any,
-    Optional,
-)
+from typing import Annotated
+from typing import Any
+from typing import Optional
 
-import pydantic
-from pydantic import (
-    BaseConfig,
-    BaseModel,
-    Field,
-)
+from pydantic import BaseModel
+from pydantic import ConfigDict
+from pydantic import Field
 
 Bool = bool
-Double = pydantic.confloat(lt=2**64)
+Double = Annotated[float, Field(lt=2 ** 64)]
 String = str
-Int32 = pydantic.conint(lt=2**32)
-Int53 = pydantic.conint(lt=2**53)
-Int64 = pydantic.conint(lt=2**64)
+Int32 = Annotated[int, Field(lt=2 ** 32)]
+Int53 = Annotated[int, Field(lt=2 ** 53)]
+Int64 = Annotated[int, Field(lt=2 ** 64)]
 Bytes = typing.Union[str, bytes]
 Vector = list
-
-ArgumentType = typing.Union[Bool, Double, String, Int32, Int53, Int64, Bytes, Vector]
 
 logger = logging.getLogger("BaseObject")
 
 
 class BaseObject(BaseModel):
-    class Config(BaseConfig):
-        arbitrary_types_allowed = True
-        anystr_strip_whitespace = True
-        underscore_attrs_are_private = True
-        use_enum_values = True
-        allow_population_by_field_name = True
-        fields = {"ID": {"default_value": None, "alias": "@type"}}
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        str_strip_whitespace=True,
+        use_enum_values=True,
+        populate_by_name=True,
+    )
 
     EXTRA: Optional[dict[str, Any]] = Field({}, alias="@extra")
