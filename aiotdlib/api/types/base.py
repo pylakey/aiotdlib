7b0b8eed3a8cc22cd30d013a5ced5ddce090626f
pylakey <pylakey@protonmail.com>
2024-02-01 17:09:41 +0300

    Update integer max/min values and field restrictions
    This commit corrects the max value of different integer types to better represent unsigned integers. It also introduces the min value restrictions for these integer types which were previously absent. Changes also include updating the pydantic field restrictions accordingly while also adding validation alias for "EXTRA" field.

--- aiotdlib/api/types/base.py
+++ aiotdlib/api/types/base.py
@@ -10,16 +10,19 @@ from pydantic import BaseModel
 from pydantic import ConfigDict
 from pydantic import Field
 
-INT32_MAX_VALUE = 2 ** 32 - 1
-INT53_MAX_VALUE = 2 ** 53 - 1
-INT64_MAX_VALUE = DOUBLE64_MAX_VALUE = 2 ** 64 - 1
+INT32_MAX_VALUE = 2 ** 31  # Unsigned INT32
+INT53_MAX_VALUE = 2 ** 52  # Unsigned INT53
+INT64_MAX_VALUE = DOUBLE64_MAX_VALUE = 2 ** 63  # Unsigned INT64
+INT32_MIN_VALUE = -INT32_MAX_VALUE
+INT53_MIN_VALUE = -INT53_MAX_VALUE
+INT64_MIN_VALUE = DOUBLE64_MIN_VALUE = -INT64_MAX_VALUE
 
 Bool = bool
 String = str
-Double = Annotated[float, Field(lt=DOUBLE64_MAX_VALUE)]
-Int32 = Annotated[int, Field(le=INT32_MAX_VALUE)]
-Int53 = Annotated[int, Field(le=INT53_MAX_VALUE)]
-Int64 = Annotated[int, Field(le=INT64_MAX_VALUE)]
+Double = Annotated[float, Field(gt=DOUBLE64_MIN_VALUE, lt=DOUBLE64_MAX_VALUE)]
+Int32 = Annotated[int, Field(gt=INT32_MIN_VALUE, lt=INT32_MAX_VALUE)]
+Int53 = Annotated[int, Field(gt=INT53_MIN_VALUE, lt=INT53_MAX_VALUE)]
+Int64 = Annotated[int, Field(gt=INT64_MIN_VALUE, lt=INT64_MAX_VALUE)]
 Bytes = typing.Union[str, bytes]
 Vector = list
 
@@ -34,4 +37,4 @@ class BaseObject(BaseModel):
         populate_by_name=True,
     )
 
-    EXTRA: Optional[dict[str, Any]] = Field({}, alias="@extra")
+    EXTRA: Optional[dict[str, Any]] = Field({}, alias="@extra", validation_alias="@extra")
